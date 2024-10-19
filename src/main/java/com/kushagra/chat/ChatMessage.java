c43ad8496e5e2e5bbbdb05d5e98f65c2fe7d6be4
Kushagra <kushagra.s2002@gmail.com>
2024-10-20 01:25:19 +0530

    my first commit for this repo

---
+++ src/main/java/com/kushagra/chat/ChatMessage.java
@@ -0,0 +1,19 @@
+package com.kushagra.chat;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage{
+    private MessageType type;
+    private String content;
+    private String sender;
+
+}
